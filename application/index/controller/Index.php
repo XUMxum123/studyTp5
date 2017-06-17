a33847b4b61dd145b7ee8310c61954379e2b466a
xum <18850337241@163.com>
2017-06-17 17:21:05 +0800

    add show => phpinfo and thinkphp version

--- application/index/controller/Index.php
+++ application/index/controller/Index.php
@@ -23,6 +23,11 @@ class Index extends Controller
         return '<style type="text/css">*{ padding: 0; margin: 0; } .think_default_text{ padding: 4px 48px;} a{color:#2E5CD5;cursor: pointer;text-decoration: none} a:hover{text-decoration:underline; } body{ background: #fff; font-family: "Century Gothic","Microsoft yahei"; color: #333;font-size:18px} h1{ font-size: 100px; font-weight: normal; margin-bottom: 12px; } p{ line-height: 1.6em; font-size: 42px }</style><div style="padding: 24px 48px;"> <h1>:)</h1><p> ThinkPHP V5<br/><span style="font-size:30px">十年磨一剑 - 为API开发设计的高性能框架</span></p><span style="font-size:22px;">[ V5.0 版本由 <a href="http://www.qiniu.com" target="qiniu">七牛云</a> 独家赞助发布 ]</span></div><script type="text/javascript" src="http://tajs.qq.com/stats?sId=9347272" charset="UTF-8"></script><script type="text/javascript" src="http://ad.topthink.com/Public/static/client.js"></script><thinkad id="ad_bd568ce7058a1091"></thinkad>';
     }
     
+    public function phpInfo()
+    {
+    	phpinfo();
+    }
+    
     // 当前模块/默认视图目录/当前控制器（小写）/当前操作（小写）.html
     /*
      * {$varname.aa	??	'xxx'}
@@ -81,6 +86,7 @@ class Index extends Controller
     	$this->assign('id',$id);
     	$this->assign('list',$list);
     	$this->assign('dimensionList',$dimensionList);
+    	$this->assign('thinkVersion',THINK_VERSION);
     	return $this->fetch();
     }
     
@@ -185,6 +191,7 @@ class Index extends Controller
     			DB_NBATEAM_WIN      => [['gt',50],['lt',60],'and']
     	];
     	$list = $nbateam->where($where)->select();
+    	//echo $Think.version;
     	dump($list);
     }
     
